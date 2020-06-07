6ed433c6d9a186205d6091bfe441db2c6a22f3ab
yhong2 <40923859+yhong2@users.noreply.github.com>
2020-06-07 09:14:28 -0700

    Update run_main.py

--- run_main.py
+++ run_main.py
@@ -42,10 +42,10 @@ print("Start ticking")
 tt = time.time()
 
 it_idx = 10
-update_cnt = 27
+update_cnt = 36
 # Number of equations
 N = 9
-K_max = 10000#10000
+K_max = 12000#10000
 
 # Final Time
 T = 4.0
